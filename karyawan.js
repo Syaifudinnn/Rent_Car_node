cf928f65413b25f398fddf62a3e8c1465ece5c1b
Mukhammad Syaifudin Adn <99938083+Syaifudinnn@users.noreply.github.com>
2023-03-13 21:28:37 +0700

    Update karyawan.js

--- karyawan.js
+++ karyawan.js
@@ -1,10 +1,51 @@
 const express = require("express")
 const router = express.Router()
+const md5 = require("md5")
+const Cryptr = require("cryptr")
+const crypt = new Cryptr("140533601726") 
+
 const db = require("./db")
+
+validateToken = () => {
+    return (req, res, next) => {
+
+        if (!req.get("Token")) {
+            res.json({
+                message: "Access Forbidden"
+            })
+        } 
+        else {
+            let token  = req.get("Token")
+            
+            // decrypt token menjadi id_user
+            let decryptToken = crypt.decrypt(token)
+
+            // sql cek id_user
+            let sql = "select * from karyawan where ?"
+
+            // set parameter
+            let param = { id_karyawan: decryptToken}
+
+            // run query
+            db.query(sql, param, (error, result) => {
+                if (error) throw error
+                if (result.length > 0) {
+                    next()
+                } 
+                else {
+                    res.json({
+                        message: "Invalid Token"
+                    })
+                }
+            })
+        }
+
+    }
+}
 
 //================================================ KARYAWAN ================================================
 //akses data karyawan
-router.get("/karyawan", (req,res) => {
+router.get("/karyawan", validateToken(), (req,res) => {
 
     let sql = "select * from karyawan"
 
@@ -60,7 +101,7 @@ router.post("/karyawan", (req,res) => {
         nama_karyawan : req.body.nama_karyawan,
         alamat_karyawan : req.body.alamat_karyawan,
         username : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password) 
     }
 
     let sql = "insert into karyawan set ?"
@@ -80,6 +121,39 @@ router.post("/karyawan", (req,res) => {
         res.json(response)
     })
 
+})
+
+// endpoint login karyawan (authentication)
+router.post("/auth", (req, res) => {
+
+    let param = [
+        req.body.username,
+        md5(req.body.password) 
+    ]
+    
+
+    // create sql query
+    let sql = "select * from karyawan where username = ? and password = ?"
+
+    // run query
+    db.query(sql, param, (error, result) => {
+        if (error) throw error
+
+        // cek jumlah data hasil query
+        if (result.length > 0) {
+            // karyawan tersedia
+            res.json({
+                message: "Logged",
+                token: crypt.encrypt(result[0].id_karyawan), // generate token
+                data: result
+            })
+        } else {
+            // karyawan tidak tersedia
+            res.json({
+                message: "Invalid username/password"
+            })
+        }
+    })
 })
 
 //mengubah data karyawan
